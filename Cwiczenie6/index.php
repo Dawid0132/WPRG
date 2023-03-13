630a9f9ebbfee4accb9782f477fe10904a144453
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2023-03-13 12:13:09 +0100

    Cwiczenie6

--- Cwiczenie6/index.php
+++ Cwiczenie6/index.php
@@ -1,14 +1,17 @@
 <?php
 
-$a = (int)readline("Podaj liczbę naturalną: ");
-$b = (int)readline("Podaj liczbę naturalną: ");
-$c = (int)readline("Podaj liczbę naturalną: ");
+$a = readline("Podaj liczbę naturalną: ");
+$b = readline("Podaj liczbę naturalną: ");
+$c = readline("Podaj liczbę naturalną: ");
 
-
-if ($a + $b > $c && $a + $c > $b && $b + $c > $a) {
-    echo "Z tych boków można utworzyć trójkąt";
-}else{
-    echo "Z tych boków nie można utworzyć trójkąta";
+if (is_numeric($a) && is_numeric($b) && is_numeric($c)) {
+    if ($a + $b > $c && $a + $c > $b && $b + $c > $a) {
+        echo "Z tych boków można utworzyć trójkąt";
+    } else {
+        echo "Z tych boków nie można utworzyć trójkąta";
+    }
+} else {
+    echo "BŁĄD";
 }
 
 ?>
