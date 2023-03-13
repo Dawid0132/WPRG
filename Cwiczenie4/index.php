e092c50d87dadd94eaf744581e27edc7cbaa8bea
s24965-pj <92219386+Dawid0132@users.noreply.github.com>
2023-03-13 11:18:28 +0100

    Cwiczenie4

---
+++ Cwiczenie4/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+
+$a = readline("Wprowadź pierwszą liczbę: ");
+$b = readline("Wprowadż drugą liczbę: ");
+
+echo "Wynik dodwania: " , $a+$b,"\n";
+echo "Wynik odejmowania: ", $a-$b,"\n";
+echo "Wynik mnożenia: ", $a*$b,"\n";
+echo "Modulo: ",$a%$b,"\n";
+
+
+?>
+</body>
+</html>
